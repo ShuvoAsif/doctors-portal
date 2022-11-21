462233b8950324580872bcd37e7b8f08fd15f161
ShuvoAsif <asif.shuvo.learning@gmail.com>
2022-11-22 03:11:57 +0600

    home

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
+import InfoCards from '../InfoCard/InfoCards';
+import MakeAppointment from '../MakeAppointment/MakeAppointment';
+import Services from '../Services/Services';
+import Testimonial from '../Testimonial/Testimonial';
 
 const Home = () => {
     return (
-        <div>
-            <h1>this is home</h1>
+        <div className='mx-5'>
+            <Banner></Banner>
+            <InfoCards></InfoCards>
+            <Services></Services>
+            <MakeAppointment></MakeAppointment>
+            <Testimonial></Testimonial>
         </div>
     );
 };
